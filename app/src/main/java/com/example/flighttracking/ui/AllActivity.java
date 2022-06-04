436811d91d425b13117fa193f5b8f86a499683ea
gilbertKorir <jerikorir@gmail.com>
2022-06-04 21:09:33 +0300

    ft: customized the all activity

--- app/src/main/java/com/example/flighttracking/ui/AllActivity.java
+++ app/src/main/java/com/example/flighttracking/ui/AllActivity.java
@@ -1,16 +1,45 @@
 package com.example.flighttracking.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 import com.example.flighttracking.R;
+import com.example.flighttracking.adapters.MyViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class AllActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    MyViewPagerAdapter myViewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all);
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager2 = findViewById(R.id.viewPager);
+        myViewPagerAdapter = new MyViewPagerAdapter(this);
+        viewPager2.setAdapter(myViewPagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
