a6a0f4b4048e700d711d4755310ef2959def1e8f
gilbertKorir <jerikorir@gmail.com>
2022-06-06 19:18:44 +0300

    ft:customized on click card

--- app/src/main/java/com/example/flighttracking/fragments/HomeFragment.java
+++ app/src/main/java/com/example/flighttracking/fragments/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.example.flighttracking.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,41 +11,24 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.flighttracking.R;
+import com.example.flighttracking.ui.AllActivity;
+import com.example.flighttracking.ui.BookingActivity;
+import com.example.flighttracking.ui.MainActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class HomeFragment extends Fragment {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class HomeFragment extends Fragment implements View.OnClickListener {
+    @BindView(R.id.book) CardView mBook;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,15 +37,21 @@ public class HomeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
+        mBook.setOnClickListener(this);
+        return view;
+    }
+    @Override
+    public void onClick(View v) {
+//            Intent intent = new Intent(, BookingActivity.class);
+//            startActivity(intent);
     }
 }
