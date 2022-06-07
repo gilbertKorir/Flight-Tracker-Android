96c7c96779bc302c82722d6035e1fa63a7246bff
gilbertKorir <jerikorir@gmail.com>
2022-06-07 18:10:57 +0300

    ft:refactored the class specific airport

--- app/src/main/java/com/example/flighttracking/fragments/FlightsFragment.java
+++ app/src/main/java/com/example/flighttracking/fragments/FlightsFragment.java
@@ -1,19 +1,37 @@
 package com.example.flighttracking.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SearchView;
 
+import com.example.flighttracking.Constants;
 import com.example.flighttracking.R;
+import com.example.flighttracking.models.portsbycountry.Response;
+import com.example.flighttracking.network.AirApi;
+import com.example.flighttracking.network.AirClient;
+import com.example.flighttracking.ui.SpecificAirportActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
 //import com.example.flighttracking.ui.FormActivity;
 
 public class FlightsFragment extends Fragment {
 
-
+    @BindView(R.id.searc_view) SearchView searchView;
+    @BindView(R.id.searchButton) Button search;
+    AirApi airApi;
 
     public FlightsFragment() {
         // Required empty public constructor
@@ -35,11 +53,34 @@ public class FlightsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_flights, container, false);
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+        airApi = AirClient.getClient();
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<Response> call = airApi.searchGif(searchView.getQuery().toString(), Constants.AIR_API_KEY);
+                call.enqueue(new Callback<Response>() {
+                    @Override
+                    public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+                        Intent intent = new Intent(getContext(), SpecificAirportActivity.class);
+                        intent.putExtra("Response", (Parcelable) response.body());
+                        startActivity(intent);
+                    }
 
+                    @Override
+                    public void onFailure(Call<Response> call, Throwable t) {
 
-}
+                    }
+                });
+
+            }
+        });
+    }
+    }
--- app/src/main/java/com/example/flighttracking/network/AirApi.java
+++ app/src/main/java/com/example/flighttracking/network/AirApi.java
@@ -2,6 +2,7 @@ package com.example.flighttracking.network;
 
 import com.example.flighttracking.models.airports.AirportsListResponse;
 import com.example.flighttracking.models.country.CountrySearchResponse;
+import com.example.flighttracking.models.portsbycountry.Response;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -12,4 +13,7 @@ public interface AirApi {
         Call<CountrySearchResponse> getCountry(@Query("api_key") String api_key);
         @GET("airports")
         Call<AirportsListResponse> getAirport(@Query("api_key") String api_key);
+        @GET("suggest")
+        Call<Response> searchGif(@Query("q") String term,
+                                 @Query("api_key") String api_key);
 }
