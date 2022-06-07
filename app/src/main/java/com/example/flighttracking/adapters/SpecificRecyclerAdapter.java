59e753184c17bf8b38d1c7ea3e5b41599cc786e9
gilbertKorir <jerikorir@gmail.com>
2022-06-07 18:40:44 +0300

    ft:refactored the class specific airport

--- app/src/main/java/com/example/flighttracking/adapters/SpecificRecyclerAdapter.java
+++ app/src/main/java/com/example/flighttracking/adapters/SpecificRecyclerAdapter.java
@@ -1,14 +1,22 @@
 package com.example.flighttracking.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.flighttracking.R;
+import com.example.flighttracking.models.country.Response;
 import com.example.flighttracking.models.portsbycountry.Airport;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SpecificRecyclerAdapter extends RecyclerView.Adapter<SpecificRecyclerAdapter.MyHolder>{
 
@@ -23,7 +31,9 @@ public class SpecificRecyclerAdapter extends RecyclerView.Adapter<SpecificRecycl
     @NonNull
     @Override
     public SpecificRecyclerAdapter.MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view= layoutInflater.inflate(R.layout.specific,parent,false);
+        return  new MyHolder(view);
     }
 
     @Override
@@ -33,6 +43,24 @@ public class SpecificRecyclerAdapter extends RecyclerView.Adapter<SpecificRecycl
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
+    }
+    class MyHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.nameView) TextView name;
+        @BindView(R.id.popularity) TextView popularity;
+        @BindView(R.id.cityCode) TextView city;
+        @BindView(R.id.countryCode) TextView country;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+        public void bindSpecific(Airport airport){
+            name.setText(airport.getName());
+            popularity.setText(airport.getPopularity());
+            city.setText(airport.getCityCode());
+            country.setText(airport.getCountryCode());
+
+        }
     }
 }
