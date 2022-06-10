7f59affb20f0e5136fc0e2803d13177125c18172
gilbertKorir <jerikorir@gmail.com>
2022-06-10 22:52:16 +0300

    ft:refactored the login activity

--- app/src/main/java/com/example/flighttracking/ui/CreateAccountActivity.java
+++ app/src/main/java/com/example/flighttracking/ui/CreateAccountActivity.java
@@ -2,13 +2,62 @@ package com.example.flighttracking.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import com.example.flighttracking.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements  View.OnClickListener {
+
+    public static final String TAG = CreateAccountActivity.class.getSimpleName();
+    @BindView(R.id.createUserButton)
+    Button mCreateUserButton;
+    @BindView(R.id.nameEditText)
+    TextInputLayout mNameEditText;
+    @BindView(R.id.emailEditText)
+    EditText mEmailEditText;
+    @BindView(R.id.passwordEditText)
+    EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText)
+    EditText mConfirmPasswordEditText;
+    @BindView(R.id.loginTextView)
+    TextView mLoginTextView;
+    @BindView(R.id.firebaseProgressBar)
+    ProgressBar mSignInProgressBar;
+    @BindView(R.id.loadingTextView)
+    TextView mLoadingSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+
+        mLoginTextView.setOnClickListener(this);
+        mCreateUserButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLoginTextView) {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+        if (v == mCreateUserButton) {
+
+//            createNewUser();
+        }
     }
 }
